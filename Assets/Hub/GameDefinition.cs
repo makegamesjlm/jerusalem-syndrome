201e54abeee189376fb71a0486e920036e71af39
Alon Tzarafi <alontzarafi@gmail.com>
2019-07-03 05:39:37 +0300

    Add very basic hub system

---
+++ Assets/Hub/GameDefinition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(menuName = "_OUR_GAME/GameDefinition")]
+public class GameDefinition : ScriptableObject
+{
+    [SerializeField]
+    public SceneAsset gameScene;
+}
---
+++ Assets/UI/BackToHubButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BackToHubButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Clicked);
+    }
+    
+    void Clicked()
+    {
+        SceneManager.LoadScene("Hub");
+    }
+}
