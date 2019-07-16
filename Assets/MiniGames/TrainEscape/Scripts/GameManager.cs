f1289bbc0af7759681ea3c9f7a398ba070042f34
Omer <OZadok@gmail.com>
2019-07-16 22:56:17 +0300

    start
    just scene with simple things

---
+++ Assets/MiniGames/TrainEscape/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace MiniGames.TrainEscape.Scripts
+{
+	public class GameManager : MonoBehaviour
+	{
+		[SerializeField] private float _gameTime;
+
+		private float _gameStartTime;
+
+		public float GamePercentage => (Time.time - _gameStartTime) / _gameTime;
+
+		private void Start()
+		{
+			
+		}
+
+		private void StartTheGame()
+		{
+			Invoke(nameof(GameOver), _gameTime);
+			_gameStartTime = Time.time;
+		}
+
+		private void GameOver()
+		{
+			// todo - if needed - check that game is not done
+			Debug.Log("Game Over");
+		}
+		
+		
+	}
+}
