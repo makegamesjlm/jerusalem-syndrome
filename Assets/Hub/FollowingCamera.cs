64ba7c7d694e7b680596c2bec6992bd01d361285
Arthur <oxenarthur@gmail.com>
2019-07-30 14:22:04 +0300

    added following camera

---
+++ Assets/Hub/FollowingCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    [SerializeField] public Transform target;
+    [SerializeField] public float smoothSpeed = 0.125f;
+    [SerializeField] public Vector3 offset;
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset; 
+        Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
