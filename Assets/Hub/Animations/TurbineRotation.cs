c57ecdee5bf015dff6f2eb2a71c1882797754a51
R2R <oxhornstarcraft@gmail.com>
2019-08-13 19:18:27 +0300

    added windmill animation and collision

---
+++ Assets/Hub/Animations/TurbineRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurbineRotation : MonoBehaviour
+{
+    [SerializeField] public float y_speed = 5f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, y_speed, 0);
+    }
+}
