d43fbc24c02a0bd5a7617f4208d76a7bd564e4d6
itamardag <itamar.dag@huji.ac.il>
2019-08-27 22:20:13 +0300

    .

---
+++ Assets/Scripts/ItamarsPlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItamarsPlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public int speed = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
