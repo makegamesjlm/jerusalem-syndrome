c95327c5616008267120fbfbe6d2e604aaf70c7d
Omer Zadok <OZadok@gmail.com>
2019-08-27 22:38:58 +0300

    controls

---
+++ Assets/MiniGames/TrainEscape/Scripts/Player/Controls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace MiniGames.TrainEscape.Scripts.Player
+{
+    [CreateAssetMenu(fileName = "Controls", menuName = "MiniGames/TrainEscape/Player/Controls")]
+    public class Controls : ScriptableObject
+    {
+        public KeyCode MoveForward;
+        public string HorizontalMove;
+        
+        public bool IsMoveForward()
+        {
+            return Input.GetKey(MoveForward);
+        }
+
+        public float HorizontalMovement()
+        {
+            return Input.GetAxis(HorizontalMove);
+        }
+    }
+}
---
+++ Assets/MiniGames/TrainEscape/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace MiniGames.TrainEscape.Scripts.Player
+{
+    public class PlayerScript : MonoBehaviour
+    {
+        [SerializeField] private Controls _controls;
+
+        private void FixedUpdate()
+        {
+            Movement();
+        }
+
+        private void Movement()
+        {
+            if (_controls.IsMoveForward())
+            {
+                //todo - move forward
+            }
+            //todo - move horizontal
+        }
+    }
+}
