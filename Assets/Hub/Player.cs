4c7e4ccd6612bc15f039e8adc3a6d8d690289579
ArthurOx <oxenarthur@gmail.com>
2019-10-02 22:16:29 +0300

    added a back to hub button to faloopi

--- Assets/Hub/Player.cs
+++ Assets/Hub/Player.cs
@@ -38,7 +38,6 @@ public class Player : MonoBehaviour, PlayerHubInput.IPlayerActions
 
     private void Update()
     {
-        Debug.Log(Time.deltaTime);
         transform.position += new Vector3(horizontal * Time.deltaTime * speed, 0.0f, vertical * Time.deltaTime * speed);
         this.GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
